04092b9a62eea86417d9d606ccebaf60e659e4d9
moxizhou <mzhou8@u.rochester.edu>
2014-10-28 19:24:50 -0700

    (fix)refactor ui route

--- client/main/main.js
+++ client/main/main.js
@@ -1,22 +1,7 @@
 angular.module('myApp.main', [] )
 
 .controller('mainController', ['$scope', '$state', function($scope, $state) {
-	$state.transitionTo('main.subviews');
-
-  $scope.current;
-
-  $scope.chooseView = function(view) {
-    $scope.current = view;
-  };
-
-  $scope.primary = function(view) {
-    if ($scope.current === undefined) {
-      $scope.current = 'home';
-    }
-    return $scope.current === view;
-  };
-
-  $scope.primary('search');
+	$state.transitionTo('main');
 
 }]);
 
--- client/monthvisual/month.js
+++ client/monthvisual/month.js
@@ -3,12 +3,20 @@
 // bubble size: proportion of viral posts = # of people with 100+points/total posts
 
 angular.module('myApp.month', [])
-	.controller('monthController', function($scope) {
+	.controller('monthController', function($scope, $http) {
 		$scope.loading = false;
+		console.log("in HERE");
 		var url = "https://hacker-news.firebaseio.com/v0";
 		$scope.searchMonth = function() {
 			$scope.loading = true;
-
+			console.log("blahhh")
+			$http({
+					method: 'GET',
+					url: url + "/item/",
+				}).success(function(posts) {
+					posts.slice(0,10);
+					console.log(posts);
+				});
 		}
 	})
 	.factory('monthFactory', function() {
