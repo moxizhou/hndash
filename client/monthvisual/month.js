b2d56715fd97b79131687579f0c186c02ad79595
moxizhou <mzhou8@u.rochester.edu>
2014-10-28 20:45:40 -0700

    basic  request to hacker news sorted by post date

--- client/monthvisual/month.js
+++ client/monthvisual/month.js
@@ -5,18 +5,41 @@
 angular.module('myApp.month', [])
 	.controller('monthController', function($scope, maxFactory) {
 		$scope.loading = false;
+		$scope.date = {};
 		$scope.searchMonth = function() {
 			$scope.loading = true;
 			maxFactory.maxPost();
 		};
 	})
 	.factory('maxFactory', function($http) {
+		var timeConverter = function(UNIX_timestamp){
+		  var a = new Date(UNIX_timestamp*1000);
+		  // var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+		  // var year = a.getFullYear();
+		  // var month = months[a.getMonth()];
+		  var date = a.getDate();
+		  var hour = a.getHours();
+		  var min = a.getMinutes();
+		  // var sec = a.getSeconds();
+		  var time = date + ' ' + hour + ' ' + min;
+		  return time;
+		};
 		var maxPost = function() {
 			$http({
 				method: 'GET',
-				url: "https://hacker-news.firebaseio.com/v0/maxitem.json",
+				url: 'https://hacker-news.firebaseio.com/v0/maxitem.json',
 			}).success(function(max) {
 				console.log(max);
+				for ( var i = max - 300; i < max + 1; i++ ) {
+					$http({
+						method: 'GET',
+						url: 'https://hacker-news.firebaseio.com/v0/item/' + i + '.json',
+					}).success(function(post) {
+						if (post.score) {
+							console.log(timeConverter(post.time));
+						}
+					});
+				};
 			});
 		};
 
